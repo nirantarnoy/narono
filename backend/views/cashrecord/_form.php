d0a4c6ecb6e880b1f5f998d705ed265da77a1da8
Niran Wangyad <niran.w@cicnetgrp.net>
2024-05-10 15:09:05 +0700

    'udpate'

--- backend/views/cashrecord/_form.php
+++ backend/views/cashrecord/_form.php
@@ -122,7 +122,7 @@ $cost_title_data = \common\models\FixcostTitle::find()->where(['type_id' => 1])-
             <div class="col-lg-3">
                 <?= $form->field($model, 'office_id')->Widget(\kartik\select2\Select2::className(), [
                     'data' => \yii\helpers\ArrayHelper::map(\backend\models\Location::find()->where(['company_id'=>$model->company_id])->all(), 'id', 'name'),
-                    'value' => $model->office_id,
+                    'selected' => 'selected',
                     'options' => [
                         'class' => 'office-select2',
                         'placeholder' => '--เลือก--',
