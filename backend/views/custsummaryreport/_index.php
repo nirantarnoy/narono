048722764cd45d43d76600dd20b7f7b3226a80ee
nirantarnoy <niran_tarlek@hotmail.com>
2024-06-17 21:44:29 +0700

    'update'

--- backend/views/custsummaryreport/_index.php
+++ backend/views/custsummaryreport/_index.php
@@ -161,11 +161,10 @@ function getLineData($customer_id, $find_year, $car_type_id)
             for ($i = 0; $i <= count($model) - 1; $i++) {
                 if ($m + 1 == $model[$i]['month']) {
                     $data[$m] = $model[$i]['cnt'];
+                    continue 2;
                 }
             }
-            if ($data[$m] == null) {
-                $data[$m] = 0;
-            }
+
         }
 
     }
