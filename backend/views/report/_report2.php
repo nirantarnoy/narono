a210baf9f1b170c65c69cea5991fde5adbe34652
nirantarnoy <niran_tarlek@hotmail.com>
2024-07-07 22:35:14 +0700

    'update'

--- backend/views/report/_report2.php
+++ backend/views/report/_report2.php
@@ -15,82 +15,105 @@ $model = \backend\models\Workqueue::find()->limit(10)->all();
     </div>
 </div>
 <br/>
-<div class="row">
-    <div class="col-lg-12">
-        <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th rowspan="2" style="text-align: center;vertical-align: middle;">วันที่</th>
-                <th rowspan="2" style="text-align: center;vertical-align: middle;">ทะเบียน</th>
-                <th rowspan="2" style="text-align: center;vertical-align: middle;">ชื่อพนักงานขับรถ</th>
-                <th rowspan="2" style="text-align: center;vertical-align: middle;">ชื่อลูกค้า</th>
-                <th colspan="2" style="text-align: center;">ปั้มใน</th>
-                <th colspan="2" style="text-align: center;">ปั๊มนอก</th>
-                <th colspan="2" style="text-align: center;">รวมน้ำมันทั้งหมด</th>
-            </tr>
-            <tr>
-                <td style="text-align: center;"><b>จำนวนลิตร</b></td>
-                <td style="text-align: center;"><b>จำนวนบาท</b></td>
-                <td style="text-align: center;"><b>จำนวนลิตร</b></td>
-                <td style="text-align: center;"><b>จำนวนบาท</b></td>
-                <td style="text-align: center;"><b>จำนวนลิตร</b></td>
-                <td style="text-align: center;"><b>จำนวนบาท</b></td>
-            </tr>
-            </thead>
-            <tbody>
-            <?php if ($model): ?>
-                <?php
-                $total_lite_all = 0;
-                $total_lite_amount = 0;
-                $total_out_lite_all = 0;
-                $total_out_lite_amount = 0;
-
-                $lint_total_lite_all = 0;
-                $lint_total_price_amount = 0;
-
-                ?>
-                <?php foreach ($model as $value): ?>
+<div id="print-area">
+    <div class="row">
+        <div class="col-lg-12">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th rowspan="2" style="text-align: center;vertical-align: middle;">วันที่</th>
+                    <th rowspan="2" style="text-align: center;vertical-align: middle;">ทะเบียน</th>
+                    <th rowspan="2" style="text-align: center;vertical-align: middle;">ชื่อพนักงานขับรถ</th>
+                    <th rowspan="2" style="text-align: center;vertical-align: middle;">ชื่อลูกค้า</th>
+                    <th colspan="2" style="text-align: center;">ปั้มใน</th>
+                    <th colspan="2" style="text-align: center;">ปั๊มนอก</th>
+                    <th colspan="2" style="text-align: center;">รวมน้ำมันทั้งหมด</th>
+                </tr>
+                <tr>
+                    <td style="text-align: center;"><b>จำนวนลิตร</b></td>
+                    <td style="text-align: center;"><b>จำนวนบาท</b></td>
+                    <td style="text-align: center;"><b>จำนวนลิตร</b></td>
+                    <td style="text-align: center;"><b>จำนวนบาท</b></td>
+                    <td style="text-align: center;"><b>จำนวนลิตร</b></td>
+                    <td style="text-align: center;"><b>จำนวนบาท</b></td>
+                </tr>
+                </thead>
+                <tbody>
+                <?php if ($model): ?>
                     <?php
-                    $line_litre = ($value->total_lite + $value->total_out_lite);
-                    $line_oil_amount = (($value->total_lite * $value->oil_daily_price) + ($value->total_out_lite * $value->oil_out_price));
+                    $total_lite_all = 0;
+                    $total_lite_amount = 0;
+                    $total_out_lite_all = 0;
+                    $total_out_lite_amount = 0;
 
-                    $total_lite_all += $value->total_lite;
-                    $total_lite_amount += ($value->total_lite * $value->oil_daily_price);
-                    $total_out_lite_all += $value->total_out_lite;
-                    $total_out_lite_amount += ($value->total_out_lite * $value->oil_out_price);
+                    $lint_total_lite_all = 0;
+                    $lint_total_price_amount = 0;
 
-                    $lint_total_lite_all += $line_litre;
-                    $lint_total_price_amount += $line_oil_amount;
                     ?>
-                    <tr>
-                        <td style="text-align: center;"><?= date('d-m-Y', strtotime($value->work_queue_date)) ?></td>
-                        <td><?= \backend\models\Car::getPlateno($value->car_id) ?></td>
-                        <td><?= \backend\models\Employee::findFullName($value->emp_assign) ?></td>
-                        <td><?= \backend\models\Customer::findCusName($value->customer_id) ?></td>
-                        <td style="text-align: right;"><?= number_format($value->total_lite, 2) ?></td>
-                        <td style="text-align: right;"><?= number_format($value->oil_daily_price, 2) ?></td>
-                        <td style="text-align: right;"><?= number_format($value->total_out_lite, 2) ?></td>
-                        <td style="text-align: right;"><?= number_format($value->oil_out_price, 2) ?></td>
-                        <td style="text-align: right;"><?= number_format($line_litre, 2) ?></td>
-                        <td style="text-align: right;"><?= number_format($line_oil_amount, 2) ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php endif; ?>
-            </tbody>
-            <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td style="text-align: right;"><b><?= number_format($total_lite_all, 2) ?></b></td>
-                <td style="text-align: right;"><b><?= number_format($total_lite_amount, 2) ?></b></td>
-                <td style="text-align: right;"><b><?= number_format($total_out_lite_all, 2) ?></b></td>
-                <td style="text-align: right;"><b><?= number_format($total_out_lite_amount, 2) ?></b></td>
-                <td style="text-align: right;"><b><?= number_format($lint_total_lite_all, 2) ?></b></td>
-                <td style="text-align: right;"><b><?= number_format($lint_total_price_amount, 2) ?></b></td>
-            </tr>
-            </tbody>
-        </table>
+                    <?php foreach ($model as $value): ?>
+                        <?php
+                        $line_litre = ($value->total_lite + $value->total_out_lite);
+                        $line_oil_amount = (($value->total_lite * $value->oil_daily_price) + ($value->total_out_lite * $value->oil_out_price));
+
+                        $total_lite_all += $value->total_lite;
+                        $total_lite_amount += ($value->total_lite * $value->oil_daily_price);
+                        $total_out_lite_all += $value->total_out_lite;
+                        $total_out_lite_amount += ($value->total_out_lite * $value->oil_out_price);
+
+                        $lint_total_lite_all += $line_litre;
+                        $lint_total_price_amount += $line_oil_amount;
+                        ?>
+                        <tr>
+                            <td style="text-align: center;"><?= date('d-m-Y', strtotime($value->work_queue_date)) ?></td>
+                            <td><?= \backend\models\Car::getPlateno($value->car_id) ?></td>
+                            <td><?= \backend\models\Employee::findFullName($value->emp_assign) ?></td>
+                            <td><?= \backend\models\Customer::findCusName($value->customer_id) ?></td>
+                            <td style="text-align: right;"><?= number_format($value->total_lite, 2) ?></td>
+                            <td style="text-align: right;"><?= number_format($value->oil_daily_price, 2) ?></td>
+                            <td style="text-align: right;"><?= number_format($value->total_out_lite, 2) ?></td>
+                            <td style="text-align: right;"><?= number_format($value->oil_out_price, 2) ?></td>
+                            <td style="text-align: right;"><?= number_format($line_litre, 2) ?></td>
+                            <td style="text-align: right;"><?= number_format($line_oil_amount, 2) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+                </tbody>
+                <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td style="text-align: right;"><b><?= number_format($total_lite_all, 2) ?></b></td>
+                    <td style="text-align: right;"><b><?= number_format($total_lite_amount, 2) ?></b></td>
+                    <td style="text-align: right;"><b><?= number_format($total_out_lite_all, 2) ?></b></td>
+                    <td style="text-align: right;"><b><?= number_format($total_out_lite_amount, 2) ?></b></td>
+                    <td style="text-align: right;"><b><?= number_format($lint_total_lite_all, 2) ?></b></td>
+                    <td style="text-align: right;"><b><?= number_format($lint_total_price_amount, 2) ?></b></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
+<br />
+<div class="row">
+    <div class="col-lg-12">
+        <div class="btn btn-default btn-print" onclick="printContent('print-area')">พิมพ์</div>
+    </div>
+</div>
+
+<?php
+$js = <<<JS
+function printContent(el)
+      {
+         var restorepage = document.body.innerHTML;
+         var printcontent = document.getElementById(el).innerHTML;
+         document.body.innerHTML = printcontent;
+         window.print();
+         document.body.innerHTML = restorepage;
+     }
+JS;
+$this->registerJs($js, static::POS_END);
+?>
+
