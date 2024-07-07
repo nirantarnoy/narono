fcb9f99c85114cc46a44acce4c2bfb41540fee6b
nirantarnoy <niran_tarlek@hotmail.com>
2024-07-07 23:03:33 +0700

    'update'

--- backend/views/report/_report2.php
+++ backend/views/report/_report2.php
@@ -111,6 +111,7 @@ if($car_search != null){
 </div>
 
 <?php
+$this->registerJsFile(\Yii::$app->request->baseUrl . '/js/jquery.table2excel.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
 $js = <<<JS
 $("#btn-export-excel").click(function(){
   $("#table-data").table2excel({
