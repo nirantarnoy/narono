6bb15635d7522c6d7a2ac6e9b4da38361bae427a
Niran Wangyad <niran.w@cicnetgrp.net>
2024-10-07 18:15:24 +0700

    'update'

--- backend/views/cashrecordreportdaily/index.php
+++ backend/views/cashrecordreportdaily/index.php
@@ -8,8 +8,8 @@ $model = null;
 $model = \common\models\QueryCashCompareReceipt::find()->where(['>=','date(trans_date)',date('Y-m-d',strtotime($from_date))])
     ->andFilterWhere(['<=','date(trans_date)',date('Y-m-d',strtotime($to_date))]);
 if($search_company_id != null){
-    echo $search_company_id;
-    $model = $model->andfilterWhere(['or','company_id' => $search_company_id,'company_id_2' => $search_company_id]);
+   // echo $search_company_id;
+    $model = $model->andfilterWhere(['or','company_idx' => $search_company_id,'company_id_2' => $search_company_id]);
 }
 if($search_office_id != null){
     $model = $model->andFilterWhere(['or','office_id' => $search_office_id,'office_id_2' => $search_office_id]);
