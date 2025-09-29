bfbc36db38ad9853b1af4efceb719ca516eed271
Niran Wangyad <niran.w@cicnetgrp.net>
2025-09-29 19:42:29 +0700

    update

--- backend/controllers/CustomerPoController.php
+++ backend/controllers/CustomerPoController.php
@@ -103,7 +103,7 @@ class CustomerPoController extends Controller
                 $model->po_file_upload = UploadedFile::getInstance($model, 'po_file_upload');
                 $model->created_at = date('Y-m-d H:i:s');
                 $model->updated_at = date('Y-m-d H:i:s');
-                if ($model->save()) {
+                if ($model->save(false)) {
                     Yii::$app->session->setFlash('success', 'สร้าง PO เรียบร้อยแล้ว');
                     return $this->redirect(['view', 'id' => $model->id]);
                 }
