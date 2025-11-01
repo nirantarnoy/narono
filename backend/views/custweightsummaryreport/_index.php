0c2ce8c1716aacc0c285d96fe7f9f940612ab24a
Niran Wangyad <niran.w@cicnetgrp.net>
2025-11-01 16:33:59 +0700

    update

--- backend/views/custweightsummaryreport/_index.php
+++ backend/views/custweightsummaryreport/_index.php
@@ -9,7 +9,7 @@ $find_month = $find_month==null?'-1':$find_month;
 $month_data = [['id' => 1, 'name' => 'มกราคม'], ['id' => 2, 'name' => 'กุมภาพันธ์'], ['id' => 3, 'name' => 'มีนาคม'], ['id' => 4, 'name' => 'เมษายน'], ['id' => 5, 'name' => 'พฤษภาคม'], ['id' => 6, 'name' => 'มิถุนายน'], ['id' => 7, 'name' => 'กรกฎาคม'], ['id' => 8, 'name' => 'สิงหาคม'], ['id' => 9, 'name' => 'กันยายน'], ['id' => 10, 'name' => 'ตุลาคม'], ['id' => 11, 'name' => 'พฤศจิกายน'], ['id' => 12, 'name' => 'ธันวาคม']];
 
 $car_type_data = \backend\models\CarType::find()->where(['status' => 1])->all();
-$work_queue_type = \yii\helpers\ArrayHelper::map(\backend\helpers\WorkQueueType::asArray(),'id','name');
+$work_queue_type = \yii\helpers\ArrayHelper::map(\backend\helpers\WorkQueueType::asArrayObject(),'id','name');
 
 $customer_data = [];
 if ($find_year != null) {
