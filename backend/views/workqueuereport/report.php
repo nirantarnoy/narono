2f0563a8688acfe50690948ead173a63fff0a61b
Niran Wangyad <niran.w@cicnetgrp.net>
2025-11-02 09:09:30 +0700

    update

--- backend/views/workqueuereport/report.php
+++ backend/views/workqueuereport/report.php
@@ -122,7 +122,7 @@ $this->registerCssFile('https://fonts.cdnfonts.com/css/calibri-light', [
         ]); ?>
 
         <div class="row">
-            <div class="col-lg-2">
+            <div class="col-lg-3">
                 <label>วันที่เริ่มต้น: </label>
                 <?= $form->field($searchModel, 'start_date')->widget(DatePicker::classname(), [
                     'options' => ['placeholder' => 'วันที่เริ่มต้น'],
@@ -132,7 +132,7 @@ $this->registerCssFile('https://fonts.cdnfonts.com/css/calibri-light', [
                     ]
                 ])->label(false) ?>
             </div>
-            <div class="col-lg-2">
+            <div class="col-lg-3">
                 <label>วันที่สิ้นสุด: </label>
                 <?= $form->field($searchModel, 'end_date')->widget(DatePicker::classname(), [
                     'options' => ['placeholder' => 'วันที่สิ้นสุด'],
@@ -142,7 +142,7 @@ $this->registerCssFile('https://fonts.cdnfonts.com/css/calibri-light', [
                     ]
                 ])->label(false) ?>
             </div>
-            <div class="col-lg-3">
+            <div class="col-lg-4">
                 <label>ลูกค้า: </label>
                 <?= $form->field($searchModel, 'customer_id')->widget(\kartik\select2\Select2::classname(), [
                       'data'=>ArrayHelper::map(\backend\models\Customer::find()->all(),'id','name'),
